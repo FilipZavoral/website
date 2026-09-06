72c7b840d9d4093c40340e8bfe7469a07fd5b11e
Honza Pobořil <bobik@ibobik.cz>
2026-09-06 21:33:34 +0000

    feat: added favicons

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,13 @@
 <template>
+  <Head>
+    <Link rel="icon" type="image/png" href="/icons/favicon-96x96.png" sizes="96x96" />
+    <Link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
+    <Link rel="shortcut icon" href="/icons/favicon.ico" />
+    <Link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+    <Meta name="apple-mobile-web-app-title" content="Jednadvacet" />
+    <Link rel="manifest" href="/icons/site.webmanifest" />
+  </Head>
+
   <UApp :locale="cs">
     <NuxtLayout>
       <NuxtRouteAnnouncer />
